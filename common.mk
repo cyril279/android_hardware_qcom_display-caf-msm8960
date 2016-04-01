4e4dbfb6ca5c8c54532f808d2461a8f5c53b7cc3
cyril279 <cyril.x9@gmail.com>
2016-04-01 09:35:30 -0400

    display-caf-msm8960: Fix build on Omni

--- common.mk
+++ common.mk
@@ -48,7 +48,7 @@ ifeq ($(TARGET_USES_QCOM_BSP),true)
 # This flag is used to compile out any features that depend on framework changes
     common_flags += -DQCOM_BSP
 endif
-ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
+ifeq ($(call is-vendor-board-platform,QCOM),true)
 # This check is to pick the kernel headers from the right location.
 # If the macro above is defined, we make the assumption that we have the kernel
 # available in the build tree.
