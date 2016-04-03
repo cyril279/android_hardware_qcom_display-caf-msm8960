aebe3798692a1b999d818b6c36d0bcb008a3953f
Cyril Wiggins <cyril.x9@gmail.com>
2016-04-03 16:13:22 -0400

    display-caf-msm8960: Update variant filtering and header paths

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-display))
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8960)
 
 display-hals := libgralloc libgenlock libcopybit libvirtual
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
